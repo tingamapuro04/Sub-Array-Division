8cc74f0e1b93d4df21a9a254a86f4b3b6855f780
ADOYO ALPHONCE <alponcemobutu100@gmail.com>
2022-08-04 20:54:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+// The link to the challenge is here below
+// https://www.hackerrank.com/challenges/the-birthday-bar/problem
 
+const all = []
+function birthday(s, d, m) {
+    // Write your code here
+    for (let x = 0; x< s.length; x++) {
+        let sub = s.slice(x, x+m)
+        all.push(sub)
+    }
+    const filtered = all.filter((item) => item.reduce((accum, currentValue) => {
+        return accum + currentValue
+    }, 0) === d)
+    
+    return filtered.length
+}
